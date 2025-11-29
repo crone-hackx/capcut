5fe1834f16b39973a2c5a0d927729bef05a53dca
TCRONEB HACKX <koder2win@gmail.com>
2025-11-29 16:39:43 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,15 +1,18 @@
-CREATE TABLE IF NOT EXISTS comments (
+DROP TABLE IF EXISTS comment_likes;
+DROP TABLE IF EXISTS comments;
+
+CREATE TABLE comments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT NOT NULL CHECK (length(username) >= 2 AND length(username) <= 20),
   text TEXT NOT NULL CHECK (length(text) >= 5 AND length(text) <= 500),
   likes INTEGER DEFAULT 0,
-  created_at TEXT NOT NOT NULL DEFAULT (datetime('now'))
+  created_at TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now'))
 );
 
-CREATE TABLE IF NOT EXISTS comment_likes (
+CREATE TABLE comment_likes (
   comment_id INTEGER NOT NULL,
   user_identifier TEXT NOT NULL,
-  liked_at TEXT NOT NULL DEFAULT (datetime('now')),
+  liked_at TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')),
   PRIMARY KEY (comment_id, user_identifier),
   FOREIGN KEY (comment_id) REFERENCES comments(id) ON DELETE CASCADE
 );
